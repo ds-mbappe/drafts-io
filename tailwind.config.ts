963010d9040aade872e59972a1d2d99b80943230
Daniel Stéphane <daniel-stephane.mbappe@byseven.co>
2024-05-16 17:46:30 +0200

    Another test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,13 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-  safelist: ['ProseMirror'],
   prefix: "",
   theme: {
     container: {
