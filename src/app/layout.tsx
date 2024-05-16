9202db9a51d96aced6ec01f88a8c0e0113139a92
Daniel Stéphane <daniel-stephane.mbappe@byseven.co>
2024-05-16 18:01:59 +0200

    Yare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./styles/globals.css";
+import "./globals.css";
 import "./../components/editor/styles/index.css"
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
