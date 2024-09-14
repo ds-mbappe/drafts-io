5321503ca4b12df284b5aa3179342dae028aec5c
Daniel Stéphane <mbappedaniel00@yahoo.fr>
2024-09-14 16:54:44 +0200

    Push again

--- src/app/models/Document.js
+++ src/app/models/Document.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(process?.env?.VERCEL_ENV === 'production' ? process.env.MONGO_DB_URI_PROD : process.env.MONGO_DB_URI_DEV);
+mongoose.connect(process?.env?.VERCEL_ENV === 'production' ? String(process.env.MONGO_DB_URI_PROD) : String(process.env.MONGO_DB_URI_DEV));
 mongoose.Promise = global.Promise
 
 const documentSchema = new Schema(
