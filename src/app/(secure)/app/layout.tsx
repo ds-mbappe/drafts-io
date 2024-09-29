9582b671ca6e68aabe26232254dd83e020bebc67
Daniel Stéphane <mbappedaniel00@yahoo.fr>
2024-09-29 16:40:40 +0200

    Push

--- src/app/(secure)/app/layout.tsx
+++ src/app/(secure)/app/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout(props: { children: React.ReactNode }) {
   const leftSidebar = useSidebar();
 
   return (
-    <main className="w-full h-full bg-content1 flex flex-col">
+    <main className="w-full h-screen bg-content1 flex flex-col">
       <Alert />
 
       {/* <NavbarApp
