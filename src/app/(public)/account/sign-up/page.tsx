b1636d0f18523b1339ed73e0168245645ec563f5
Daniel MBAPPE <mbappedaniel00@yahoo.fr>
2025-07-16 22:09:39 +0200

    fix: fix heigh issues in auth pages | test

--- src/app/(public)/account/sign-up/page.tsx
+++ src/app/(public)/account/sign-up/page.tsx
@@ -71,8 +71,8 @@ export default function SignInPage() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5">
-      <div className="w-[350px] sm:w-full max-w-[400px] h-full flex flex-col p-6 gap-6 bg-content1 border border-divider rounded-2xl">
+    <main className="flex h-[100dvh] flex-col items-center justify-center gap-5">
+      <div className="w-[350px] sm:w-full max-w-[400px] flex flex-col p-6 gap-6 bg-content1 border border-divider rounded-2xl">
         <div className="flex flex-col gap-6">
           {/* Sign up intro */}
           <div className="flex flex-col gap-1">
